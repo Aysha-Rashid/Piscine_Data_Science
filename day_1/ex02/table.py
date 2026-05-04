eb62030b28f703f0ebcfa530396068b6343e45ff
Aysha Al Rashid <130707249+Aysha-Rashid@users.noreply.github.com>
2026-05-04 14:21:44 +0400

    Add files via upload

---
+++ day_1/ex02/table.py
@@ -0,0 +1,71 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from psycopg2 import sql
+
+
+def create_script(table_name):
+    create_script = sql.SQL('''CREATE TABLE {} (
+                        event_time   TIMESTAMPTZ,
+                        event_type   TEXT,
+                        product_id   INTEGER,
+                        price        NUMERIC,
+                        user_id      BIGINT,
+                        user_session UUID
+                    )''').format(sql.Identifier(table_name))
+    return create_script
+
+
+def connection():
+    load_dotenv()
+    hostname = "localhost"
+    database = os.getenv('POSTGRES_DB')
+    username = os.getenv('POSTGRES_USER')
+    pwd = os.getenv('POSTGRES_PASSWORD')
+    port_id = os.getenv('PORT_ID')
+    conn = psycopg2.connect(
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id
+    )
+    return conn
+
+
+def store_in_database(filename, conn, cur):
+    assert os.path.exists(filename), "The file doesnt exist"
+    assert filename.endswith('.csv'), "File isnt csv file"
+    pos_data = filename.find("data_")
+    table_name = filename[pos_data:-4]
+    cur.execute(sql.SQL('DROP TABLE IF EXISTS {}').format(sql.Identifier(table_name)))
+    cur.execute(create_script(table_name))
+
+    with open(filename, 'r') as f:
+        next(f)  # skip header
+        cur.copy_expert(
+            sql.SQL("COPY {} FROM STDIN WITH CSV HEADER").format(sql.Identifier(table_name)), f
+        )
+
+
+def main():
+    try:
+        conn = None
+        cur = None
+        conn = connection()
+        cur = conn.cursor()
+        store_in_database("../Data/customer/data_2022_oct.csv", conn, cur)
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.commit()
+            conn.close()
+
+
+if __name__ == "__main__":
+    main()
+else:
+    print("incorrect main import")
