17534f37e8ed6de33923dafd47d368dfb8808ffa
Aysha Al Rashid <ayal-ras@lab3r1s5.42abudhabi.ae>
2026-08-27 15:19:42 +0400

    day_2, example_env, gitignore

---
+++ day_2/ex01/customers_table.py
@@ -0,0 +1,92 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from psycopg2 import sql
+import sys
+
+
+def create_script(table_name):
+    create_script = sql.SQL('''CREATE TABLE {} (
+                        event_time   TIMESTAMPTZ,
+                        event_type   TEXT,
+                        product_id   INTEGER,
+                        price        NUMERIC,
+                        user_id      BIGINT,
+                        user_session UUID
+                    )''').format(sql.Identifier(table_name))
+    return create_script
+
+
+def store_in_database(filename, conn, cur):
+    assert os.path.exists(filename), "The file doesnt exist"
+    assert filename.endswith('.csv'), "File isnt csv file"
+    pos_data = filename.find("data_")
+    table_name = filename[pos_data:-4]
+    cur.execute(sql.SQL('DROP TABLE IF EXISTS {}').format
+                (sql.Identifier(table_name)))
+    cur.execute(create_script(table_name))
+
+    with open(filename, 'r') as f:
+        next(f)  # skip header
+        cur.copy_expert(
+            sql.SQL("COPY {} FROM STDIN WITH CSV HEADER").format
+            (sql.Identifier(table_name)), f
+        )
+
+
+def connection():
+    load_dotenv()
+    hostname = "localhost"
+    database = os.getenv('POSTGRES_DB')
+    username = os.getenv('POSTGRES_USER')
+    pwd = os.getenv('POSTGRES_PASSWORD')
+    port_id = os.getenv('PORT_ID')
+    conn = psycopg2.connect(
+        host=hostname,
+        dbname=database,
+        user=username,
+        password=pwd,
+        port=port_id
+    )
+    return conn
+
+def create_customer_tables():
+    create_script = sql.SQL('''CREATE TABLE customers AS(
+                        SELECT * FROM data_2022_dec
+                        UNION ALL
+                        SELECT * FROM data_2022_nov
+                        UNION ALL 
+                        SELECT * FROM data_2022_oct
+                        UNION ALL
+                        SELECT * FROM data_2023_feb
+                        UNION ALL 
+                        SELECT * FROM data_2023_jan
+                    )''')
+    return create_script
+
+def main():
+    conn = None
+    cur = None
+    try:
+        assert len(sys.argv) != 3, "Less arguments"
+        path, dirs, files = next(os.walk(sys.argv[1]))
+        conn = connection()
+        cur = conn.cursor()
+        for f in files:
+            store_in_database(os.path.join(path, f), conn, cur)
+        customer_table = create_customer_tables()
+        cur.execute(customer_table)
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.commit()
+            conn.close()
+
+
+if __name__ == "__main__":
+    main()
+else:
+    print("incorrect main import")
